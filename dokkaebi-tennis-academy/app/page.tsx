e468bd58e47dcaa9bcdabac130eb960e873a680c
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-04-24 18:31:59 +0900

    Next.js 설치

---
+++ dokkaebi-tennis-academy/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">도깨비 테니스 아카데미!</h1>
+    </main>
+  );
+}
