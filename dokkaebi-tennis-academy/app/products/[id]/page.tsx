a48f2e3d28d97aac4550e0c2d12cf83f2f629fb1
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-06-16 19:39:30 +0900

    🐛  상품 상세 페이지에서 파라미터 처리 개선 - params.id 대신 id 변수를 사용하여 코드 가독성 향상

--- dokkaebi-tennis-academy/app/products/[id]/page.tsx
+++ dokkaebi-tennis-academy/app/products/[id]/page.tsx
@@ -3,9 +3,11 @@ import clientPromise from '@/lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 export default async function ProductDetailPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
   const client = await clientPromise;
   const db = client.db();
-  const product = await db.collection('products').findOne({ _id: new ObjectId(params.id) });
+
+  const product = await db.collection('products').findOne({ _id: new ObjectId(id) });
 
   if (!product) {
     return <div className="p-6 text-red-500 font-bold">상품을 찾을 수 없습니다</div>;
