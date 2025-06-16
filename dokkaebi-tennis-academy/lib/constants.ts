3db9c7986a7bc46707661783945325aab837f5cf
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-06-16 18:59:43 +0900

    엑세스토큰 시간 수정

--- dokkaebi-tennis-academy/lib/constants.ts
+++ dokkaebi-tennis-academy/lib/constants.ts
@@ -2,5 +2,5 @@ export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'your-acce
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'your-refresh-token-secret';
 
 // 초 단위 (JWT와 쿠키 모두에 사용 가능)
-export const ACCESS_TOKEN_EXPIRES_IN = 60; // 1시간
+export const ACCESS_TOKEN_EXPIRES_IN = 60 * 60; // 1시간
 export const REFRESH_TOKEN_EXPIRES_IN = 60 * 60 * 24 * 7; // 7일
