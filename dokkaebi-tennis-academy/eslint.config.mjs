eec2d7ad900a512cefff99fc1c80372ff4619cab
baxteryhs1118 <baxteryhs1118@gmail.com>
2026-02-05 16:40:43 +0900

    🚀 깃허브 액션에서 정상적으로 lint 단계를 통과하도록 작업 - eslint-config-next 버전을 Next.js 버전과 맞춤으로써 내부 플러그인(React Hooks 등) 간의 충돌을 해결 - FlatCompat을 사용할 때 발생할 수 있는 객체 참조 구조를 단순화하여 Converting circular structure to JSON 오류를 방지

--- dokkaebi-tennis-academy/eslint.config.mjs
+++ dokkaebi-tennis-academy/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  //검사 제외 대상 설정
+  {
+    ignores: ['.next/**', 'dist/**', 'node_modules/**'],
+  },
+  // Next.js 및 TypeScript 설정 로드
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  // 추가 규칙 (필요 시)
+  {
+    rules: {
+      // 여기에 커스텀 규칙을 추가
+    },
+  },
 ];
 
 export default eslintConfig;
