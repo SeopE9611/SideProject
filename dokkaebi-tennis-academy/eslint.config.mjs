b14ce64765043b655bc8c613e481c15688e0b8ed
baxteryhs1118 <baxteryhs1118@gmail.com>
2026-02-05 17:54:09 +0900

    수정

--- dokkaebi-tennis-academy/eslint.config.mjs
+++ dokkaebi-tennis-academy/eslint.config.mjs
@@ -1,3 +1,5 @@
+// dokkaebi-tennis-academy/eslint.config.mjs
+
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 
@@ -9,11 +11,23 @@ const eslintConfig = [
     ignores: ['.next/**', 'node_modules/**'],
   },
   ...compat.config({
-    extends: ['next/core-web-vitals'],
+    extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
+      // 기본 ESLint 규칙 OFF
       'no-empty': 'off',
       'no-unused-vars': 'off',
-      // "import/no-anonymous-default-export": "off"
+      
+      // TypeScript 환경의 'no-unused-vars' 규칙 OFF
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      // `'` 사용 시 발생하는 오류 비활성화
+      'react/no-unescaped-entities': 'off',
+
+      // React Hook 의존성 경고 비활성화
+      'react-hooks/exhaustive-deps': 'off',
+
+      // 익명 기본 내보내기 경고 비활성화 
+      'import/no-anonymous-default-export': 'off',
     },
   }),
 ];
