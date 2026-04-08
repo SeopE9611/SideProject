b12475c49182f8ff4ed6d106af50556b0630adc5
SeopE9611 <baxteryhs1118@gmail.com>
2026-04-08 20:20:15 +0900

    chore: 중복 route loading owner 제거

--- TennisFlowShop/app/admin/notifications/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { FullPageSpinner } from "@/components/system/loading";
-
-export default function Loading() {
-  return <FullPageSpinner label="알림함으로 이동 중..." minHeightClassName="min-h-[40svh]" />;
-}
