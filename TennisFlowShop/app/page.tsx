9b2182e3dc794d7cc3383e3e886234b651757407
baxteryhs1118 <baxteryhs1118@gmail.com>
2026-04-28 22:41:23 +0900

    메타 수정

--- TennisFlowShop/app/page.tsx
+++ TennisFlowShop/app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import HomePageClient from "./HomePageClient";
 
 export const metadata: Metadata = {
-  title: "홈",
+  title: "홈 | 도깨비테니스",
 };
 
 export default function Page() {
